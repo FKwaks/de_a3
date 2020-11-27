2f9dd9d924d9464a6af55f999ed3c0793380401a
Kwaks <f.m.kwaks@student.tue.nl>
2020-11-27 16:43:07 +0100

    hopelijk API erin gezet

---
+++ main.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from flask import Flask, json, request, Response
+import _pickle as cPickle
+from google.cloud import storage
+from urllib.parse import urlparse
+
+
+from notebooks import feature_engineer
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+#test
+@app.route('/', methods=['GET'])
+def test():
+    return {'hello':'world'}
+
+@app.route('/predict/', methods=['POST'])
+def predict_perf():
+    content = request.get_json()
+    df = pd.read_json(json.dumps(content), orient='records')
+    df_features = feature_engineer.clean_data(df)
+    
+    model_store_path = 'gs://de_a3/model_store/tuned/tuned_gbr.pickle'
+    
+    parse = urlparse(url=model_store_path, allow_fragments = False)
+    if parse.path[0] =='/':
+        model_path = parse.path[1:]
+    client = storage.Client()
+    bucket = client.get_bucket(parse.netloc)
+    blob = bucket.get_blob(model_path)
+    if blob is None:
+        raise AttributeError('No files to download') 
+    model_bytestream = BytesIO(blob.download_as_string())
+    model = cPickle.load(model_bytestream)
+    
+    x_predict = df_features[df_features.columns[2:]]
+    js = model.predict(x_predict)
+    resp = Response(js, status=200, mimetype='application/json')
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.headers['Access-Control-Allow-Methods'] = 'POST'
+    resp.headers['Access-Control-Max-Age'] = '1000'
+    return resp
+
+app.run(host='0.0.0.0', port=5000)
---
+++ notebooks/feature_engineer.py
@@ -0,0 +1,24 @@
+import json
+import os
+
+from keras.models import load_model
+import pickle
+from sklearn.cluster import KMeans
+
+def clean_data(dataset):
+    X = dataset[dataset.columns[2:]]
+    engine = dataset.iloc[:,0].to_list()
+    cycle = dataset.iloc[:,1].to_list()
+
+    features = dataset.columns[2:]
+    for feature in features:
+        # Creating min, max and delta variables
+        X['max_' + feature] = dataset.groupby('engine_id')[feature].cummax()
+        X['min_' + feature] = dataset.groupby('engine_id')[feature].cummin()
+
+        X['delta_' + feature] = dataset.groupby('engine_id')[feature].diff()
+        X['delta_' + feature].fillna(0, inplace=True)
+
+    X.insert(0, 'engine_id', engine)
+    X.insert(1, 'cycle', cycle)
+    return X
