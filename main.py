15b780e1d743810517f9e7b26b17940cef457956
Jari Burgers <jari.burgers@gmail.com>
2020-11-29 16:17:47 +0000

    chagned flask

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from flask import Flask, json, request, Response
+from flask import Flask, json, request, Response, stream_with_context
 import _pickle as cPickle
 from google.cloud import storage
 from urllib.parse import urlparse
@@ -37,7 +37,7 @@ def predict_perf():
     
     x_predict = df_features[df_features.columns[2:]]
     js = model.predict(x_predict)
-    resp = Response(js, status=200, mimetype='application/json')
+    resp = Response(stream_with_context(js), status=200, mimetype='application/json')
     resp.headers['Access-Control-Allow-Origin'] = '*'
     resp.headers['Access-Control-Allow-Methods'] = 'POST'
     resp.headers['Access-Control-Max-Age'] = '1000'
